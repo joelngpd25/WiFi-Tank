1c5101616bb5075953a837ff123825d66ddca336
Joel <joelng_pd25@live.com>
2016-06-16 20:00:13 +0800

    Create WiFiTank.ino

---
+++ WiFiTank.ino
@@ -0,0 +1,26 @@
+/* Set these to your desired credentials. */
+const char *ssid = "xxx";
+const char *password = "xxx";
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+WiFiServer server(80);
+
+
+void setup(void) {  
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  // Start TCP server.
+  server.begin();
+}
+
+void loop() {
+    WiFiClient client = server.available();
+    if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        Serial.println(client.read());
+      }
+    }
+    client.stop();
+  }
+}
